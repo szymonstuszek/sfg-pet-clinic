11e3eda56e4251c48ef7784e097232b382925f04
szymonstuszek <szymonstuszek@gmail.com>
2020-08-10 13:15:40 +0200

    closes #44

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialtyRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialtyRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}
